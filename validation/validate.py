7da0a11b3d98848f83367fcf27be4f184e3a54b8
Lev Konstantinovskiy <lev.konst@gmail.com>
2022-11-18 11:57:18 +0100

    typo

--- validation/validate.py
+++ validation/validate.py
@@ -56,8 +56,8 @@ def read_and_validate(csv_file: str = "data/train.csv") -> HouseList:
     df = pd.read_csv(
         csv_file,
         usecols=["Id", "Street", "YearBuilt", "Fireplaces", "FireplaceQu", "1stFlrSF", "2ndFlrSF"],
-    )
-    # News to convert pandas float nan to None so that Pydantic Optional works.
+    )   
+    # convert pandas float nan to None so that Pydantic Optional works.
     df = df.replace({np.nan: None})
     houses = [House(**house_dict) for house_dict in df.to_dict("index").values()]
     house_list = HouseList(houses=houses)
